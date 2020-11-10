e638d8c210a32c75aabcf962c7796c2bde63d03e
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-11-10 12:15:00 -0600

    Initial commit

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>Hello from Home</h1>
+    </div>
+  );
+}
+
+export default Home;
