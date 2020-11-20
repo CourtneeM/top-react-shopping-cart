dee2607c34c0b45a1527f211de003cd955e3d6b2
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-11-20 14:46:46 -0600

    Implement increment and decrement shopping cart buttons

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,25 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Shop = () => {
-  const products = [{id: 1, image: '#'}, {id: 2, image: '#'}, {id: 3, image: '#'}, {id: 4, image: '#'}, {id: 5, image: '#'}];
+  const [cartTotal, setCartTotal] = useState(0);
+  
+  const products = [
+    {id: 1, image: '#'}, 
+    {id: 2, image: '#'}, 
+    {id: 3, image: '#'}, 
+    {id: 4, image: '#'}, 
+    {id: 5, image: '#'}
+  ];
+
+  const [cart, setCart] = useState([]);
+
+  
+  const decrementCartTotal = (id, index) => {
+    if (cartTotal === 0) {
+      return;
+    }
+    setCartTotal((prevCartTotal) => prevCartTotal - 1);
+
+    let updatedCart = [...cart];
+    updatedCart[index] = {
+      id,
+      total: updatedCart[index].total - 1
+    }
+
+    setCart(updatedCart);
+  }
+  
+  function incrementCartTotal(id, index) {
+    setCartTotal((prevCartTotal) => prevCartTotal + 1);
+    
+    let updatedCart = [...cart];
+    if (updatedCart[index] === undefined) {
+      updatedCart[index] = {total: 0}
+    }
+    updatedCart[index] = {
+      id,
+      total: updatedCart[index].total + 1
+    }
+    setCart(updatedCart);
+  }
 
   return (
     <div className="shop">
-      <header>
+      <header style={{position: "relative"}}>
         <h1>Nal Hutta Trading Post</h1>
         <Link to="/cart">
           <i className="fas fa-shopping-bag fa-2x"></i>
         </Link>
+        <p style={{position: "absolute", bottom: "-25px", left: "30px"}}>{cartTotal}</p>
       </header>
       <main>
         <p>Inventory Varies Daily</p>
         <div className="products">
-          {products.map(({id, image}) => (
+          {products.map(({id, img}, index) => (
             <div>
               {id}
-              <i class="fas fa-minus"></i>
-              <i class="fas fa-plus"></i>
+              <br />
+              <i onClick={() => decrementCartTotal(id, index)} class="fas fa-minus"></i>
+              <i onClick={() => incrementCartTotal(id, index)} class="fas fa-plus"></i>
             </div>
           ))}
         </div>
