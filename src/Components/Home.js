a2b25f33323182b0930cf81077764a67222e15fd
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-12-09 18:50:32 -0600

    Add styling to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,11 +3,32 @@ import { Link } from 'react-router-dom';
 import '../Styles/Home.css';
 
 const Home = () => {
+  const styles = {
+    homeContainer: {
+      color: '#eee',
+      backgroundColor: '#333',
+    },
+    title: {
+      fontSize: '2.6rem',
+    },
+    enterBtn: {
+      padding: '15px 30px',
+      fontSize: '1.4rem',
+      fontWeight: '600',
+      backgroundColor: 'yellow',
+      border: 'none',
+      borderRadius: '7px',
+      cursor: 'pointer',
+    }
+  }
+
   return (
-    <div className="Home">
-      <h1>Nal Hutta Trading Post</h1>
+    <div className="Home" style={styles.homeContainer}>
+      <h1 style={styles.title}>Nal Hutta Trading Post</h1>
       <main>
-        <h3><Link to="/shop">Enter</Link></h3>
+        <Link to="/shop">
+          <button style={styles.enterBtn}>Enter</button>
+        </Link>
       </main>
     </div>
   );
